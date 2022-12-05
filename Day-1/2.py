b23eea35451f407eabfff1f6116559902ceb9bd3
Aftaab Siddiqui <aftaab2507@gmail.com>
2022-12-05 12:39:09 +0530

    Day-1: part-2

---
+++ Day-1/2.py
@@ -0,0 +1,15 @@
+f = open("input.txt", "r")
+
+elf_calories = []
+curr = 0
+
+for x in f:
+  	if x == "\n":
+  		elf_calories.append(curr)
+  		curr = 0
+  	else:
+  		curr += int(x)
+
+elf_calories.sort(reverse=True)
+
+print(sum(elf_calories[0:3]))
